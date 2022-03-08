2c663d09049088c51d5db1c215eb47aac1f3aee8
KarenElaine <karenelaine.stewart@yahoo.com>
2022-03-08 09:03:36 -0500

    created book model

--- books/models.py
+++ books/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    def __repr__(self):
-        return f"<User username={self.username}>"
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.title
+
+
+class User(AbstractUser):
+    def __repr__(self):
+       return f"<User username={self.username}>"
+#
+    def __str__(self):
+       return self.username
